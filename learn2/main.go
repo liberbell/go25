2cc489a4770f0cd6fbb6b88e0547700a64a82094
Liberbell <liberbell@gmail.com>
2023-05-18 08:29:35 +0900

    main2

--- learn2/main.go
+++ learn2/main.go
@@ -7,8 +7,11 @@ func main() {
 	myString = "Green"
 
 	log.Println("mystring is set to ", myString)
+
+	changeUsingPointer(&myString)
 }
 
 func changeUsingPointer(s *string) {
 	newValue := "Red"
+	*s := newValue
 }
