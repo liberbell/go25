47bfd79f3a5e8dc4a69b0a4e604fcbab16e6601f
Liberbell <liberbell@gmail.com>
2023-05-18 08:39:57 +0900

    main3

--- learn3/main.go
+++ learn3/main.go
@@ -6,10 +6,9 @@ var s = "seven"
 
 func main() {
 	var s2 = "six"
-	log.Println(s)
+	log.Println("s  is", s)
 	log.Println("s2 is", s2)
 
-	//comment
 }
 
 func saySomething(s string) (string, string) {
