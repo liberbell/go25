5a6c39c38fbab14e662760927cf58091102c910a
Liberbell <liberbell@gmail.com>
2023-05-19 08:35:56 +0900

    main5

--- learn4/main.go
+++ learn4/main.go
@@ -6,16 +6,18 @@ import (
 )
 
 type User struct {
-	FirstName string
-	LastName  string
-	Age       int
-	BirthDate time.Time
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
 }
 
 func main() {
 	user := User{
-		FirstName: "Bob",
-		LastName:  "Mary",
+		FirstName:   "Bob",
+		LastName:    "Mary",
+		PhoneNumber: "111-222-3333",
 	}
 
 	log.Println(user.FirstName, user.LastName, "BirthDate:", user.BirthDate)
