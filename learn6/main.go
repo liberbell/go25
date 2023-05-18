55f4dbdde024fe408c56650563e7013a6e333f1f
Liberbell <liberbell@gmail.com>
2023-05-19 08:48:44 +0900

    main2

--- learn6/main.go
+++ learn6/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "log"
+
 func main() {
 	var myString string
 	var myInt int
 
 	myString = "hi"
 	myInt = 11
+
+	mySecondString := "another string"
+
+	log.Println(myString, myInt, mySecondString)
 }
