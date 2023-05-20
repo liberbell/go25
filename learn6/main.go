5d4c090f28a3c383db4a0a2beb04bfc53f8f6bdc
Liberbell <liberbell@gmail.com>
2023-05-20 10:47:23 +0900

    main15

--- learn6/main.go
+++ learn6/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"log"
-	"sort"
 )
 
 func main() {
-	var mySlice []int
-	mySlice = append(mySlice, 5)
-	mySlice = append(mySlice, 1)
-	mySlice = append(mySlice, 3)
+	// var mySlice []int
+	// mySlice = append(mySlice, 5)
+	// mySlice = append(mySlice, 1)
+	// mySlice = append(mySlice, 3)
 
-	sort.Ints(mySlice)
+	// sort.Ints(mySlice)
 
-	log.Println(mySlice)
+	// log.Println(mySlice)
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	log.Println(numbers)
 }
