a2eecee525bbea80bfb4e5a486692494fce4ad38
Liberbell <liberbell@gmail.com>
2023-05-20 10:34:25 +0900

    main10

--- learn6/main.go
+++ learn6/main.go
@@ -2,47 +2,9 @@ package main
 
 import "log"
 
-type User struct {
-	FirstName string
-	LastName  string
-}
-
 func main() {
-	// var myString string
-	// var myInt int
+	var myString string
+	myString = "Fish"
 
-	// myString = "hi"
-	// myInt = 11
-
-	// mySecondString := "another string"
-
-	// log.Println(myString, myInt, mySecondString)
-
-	// myMap := make(map[string]int)
-
-	// myMap["dog"] = "Eric"
-	// myMap["other-dog"] = "Bob"
-	// myMap["dog"] = "Alex"
-
-	// log.Println(myMap["dog"])
-	// log.Println(myMap["other-dog"])
-
-	// myMap["First"] = 1
-	// myMap["Second"] = 2
-
-	// log.Println(myMap["First"], myMap["Second"])
-
-	myMap := make(map[string]User)
-
-	me := User{
-		FirstName: "Bob",
-		LastName:  "Mary",
-	}
-
-	myMap["me"] = me
-	log.Println(myMap["me"].FirstName)
-
-	var myNewVar float32
-	myNewVar = 11.1
-
+	log.Println(myString)
 }
