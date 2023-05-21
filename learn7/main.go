65f1e8b64a9aa01e2611d57226f04bbba8de414c
Liberbell <liberbell@gmail.com>
2023-05-21 14:02:27 +0900

    main4

--- learn7/main.go
+++ learn7/main.go
@@ -12,12 +12,20 @@ func main() {
 	// 	log.Println("isTrue is ", isTrue)
 	// }
 
-	cat := "cat"
+	// cat := "cat2"
 
-	if cat == "cat" {
-		log.Println("Cat is cat")
+	// if cat == "cat" {
+	// 	log.Println("Cat is cat")
+	// } else {
+	// 	log.Println("Cat is not cat")
+	// }
+
+	myNum := 100
+	isTrue := false
+
+	if myNum > 99 && isTrue == false {
+		log.Println("Mynum is greater than 99 and istrue is set to true")
 	} else {
-		log.Println("Cat is not cat")
-	}
 
+	}
 }
