c8941f3b14ef74eeb53eda6f03fb244ee7dc381c
Liberbell <liberbell@gmail.com>
2023-05-21 13:24:12 +0900

    main2

--- learn6/main.go
+++ learn6/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"log"
-)
-
 func main() {
 	// var mySlice []int
 	// mySlice = append(mySlice, 5)
@@ -14,8 +10,9 @@ func main() {
 
 	// log.Println(mySlice)
 
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	log.Println(numbers)
-	log.Println(numbers[0:2])
+	// log.Println(numbers)
+	// log.Println(numbers[0:2])
+	// log.Println(numbers[6:9])
 }
