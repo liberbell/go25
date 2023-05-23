16804aa44e6eca3c526b297140c0ce108d02c634
Liberbell <liberbell@gmail.com>
2023-05-24 08:41:06 +0900

    main7

--- learn10/main.go
+++ learn10/main.go
@@ -14,20 +14,20 @@ type Person struct {
 
 func main() {
 	myJson := `
-	[
-		{
-			"first_name: "Bob",
-			"last_name": "Mary",
-			"hair_color": "brown",
-			"has_dog": false,
-		},
-		{
-			"first_name": "Elton",
-			"last_name": "John",
-			"hair_color": "gold",
-			"has_dog": true,
-		}
-	]`
+[
+	{
+		"first_name: "Bob",
+		"last_name": "Mary",
+		"hair_color": "brown",
+		"has_dog": false,
+	},
+	{
+		"first_name": "Elton",
+		"last_name": "John",
+		"hair_color": "gold",
+		"has_dog": true,
+	}
+]`
 
 	var unmarshaled []Person
 
