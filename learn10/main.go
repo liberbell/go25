cd26f3b8991c47c421bfe87e25915ba1c563ac5c
Liberbell <liberbell@gmail.com>
2023-05-24 08:32:57 +0900

    main2

--- learn10/main.go
+++ learn10/main.go
@@ -3,10 +3,18 @@ package main
 
 func main()  {
 	myJson := `
-	{
-		"first_name: "Bob",
-		"last_name": "Mary",
-		"hair_color": "brown",
-		"has_dog": false,
-	}`
+	[
+		{
+			"first_name: "Bob",
+			"last_name": "Mary",
+			"hair_color": "brown",
+			"has_dog": false,
+		},
+		{
+			"first_name": "Elton",
+			"last_name": "John",
+			"hair_color": "gold",
+			"has_dog": true,
+		}
+	]`
 }
