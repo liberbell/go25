1bda0833fc56eb558013609b1e478eaa854200b7
Liberbell <liberbell@gmail.com>
2023-05-24 08:38:00 +0900

    main5

--- learn10/main.go
+++ learn10/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "encoding/json"
+
 type Person struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
@@ -25,4 +27,6 @@ func main() {
 	]`
 
 	var unmarshaled []Person
+
+	err := json.Unmarshal([]byte(myJson), &unmarshaled)
 }
