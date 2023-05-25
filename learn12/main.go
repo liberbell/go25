f4107eea3724f8fa733b18e8c46659ff83e3f383
Liberbell <liberbell@gmail.com>
2023-05-26 08:51:10 +0900

    main6

--- learn12/main.go
+++ learn12/main.go
@@ -9,6 +9,9 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		n, err := fmt.Fprintf(w, "Hello world")
+		if err != nil {
+			fmt.Println(err)
+		}
 		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 	})
 }
