cd0576a3e823f41f5d0ce7561260191d8ac4544f
Liberbell <liberbell@gmail.com>
2023-05-26 08:43:44 +0900

    main3

--- learn12/main.go
+++ learn12/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 
-	http.HandlerFunc("/", func(w http.ResponseWriter, r *http.Request) {})
+	http.HandlerFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello world")
+	})
 }
