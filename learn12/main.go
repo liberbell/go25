9649606a891114ebe794c672857a8d0a63b28c21
Liberbell <liberbell@gmail.com>
2023-05-27 19:42:24 +0900

    main7

--- learn12/main.go
+++ learn12/main.go
@@ -11,7 +11,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func About(w http.ResponseWriter, r *http.Request) {
 	sum, _ := AddValue(2, 2)
-	fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
 func AddValue(x, y int) (int, error) {
