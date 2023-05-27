cde481db8378a2eb82d0b75912278b9c8482a1f4
Liberbell <liberbell@gmail.com>
2023-05-27 19:34:20 +0900

    main3

--- learn12/main.go
+++ learn12/main.go
@@ -9,9 +9,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page.")
 }
 
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page.")
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
