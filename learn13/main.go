d7c86c8b1d3c391b8b9a7e875df1ffbfd74d093e
Liberbell <liberbell@gmail.com>
2023-05-29 08:52:06 +0900

    main2

--- learn13/main.go
+++ learn13/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	_ "github.com/liber/myapp/handlers"
 )
 
 const portNumber = ":8080"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlrs.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
