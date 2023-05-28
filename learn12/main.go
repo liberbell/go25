8dc1bcb532f994eef6927bb0cd2bc0dc6c0dbbf5
Liberbell <liberbell@gmail.com>
2023-05-28 14:15:35 +0900

    main4

--- learn12/main.go
+++ learn12/main.go
@@ -18,6 +18,10 @@ func About(w http.ResponseWriter, r *http.Request) {
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error parsing template: ", err)
+	}
 }
 
 func main() {
