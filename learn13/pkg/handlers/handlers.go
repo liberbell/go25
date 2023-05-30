3a0b0454b9d75e4cb5fe4dbb6f38ce6f62dce59b
Liberbell <liberbell@gmail.com>
2023-05-30 09:03:22 +0900

    handlers5

--- learn13/pkg/handlers/handlers.go
+++ learn13/pkg/handlers/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/tsawler/go-course/pkg/render/"
+	"github.com/tsawler/go-course/pkg/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
